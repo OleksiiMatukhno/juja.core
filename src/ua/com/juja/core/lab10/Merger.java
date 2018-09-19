0dc3f1dd1e0fccc986ed896896ec1175e282d72e
OleksiiMatukhno <alexei.matukhno@gmail.com>
2018-09-19 14:21:55 +0300

    lab10 цикл while заменен на for

--- src/ua/com/juja/core/lab10/Merger.java
+++ src/ua/com/juja/core/lab10/Merger.java
@@ -1,23 +1,23 @@
 package ua.com.juja.core.lab10;
 
 public class Merger {
-    public static int[] merge(int[] fst, int[] snd) {
+    public static int[] mergeInvert(int[] fst, int[] snd) {
         int[] result = new int[fst.length + snd.length];
         int fstIndex = 0;
         int sndIndex = 0;
-        while (fstIndex + sndIndex != result.length) {
-            if (fst.length == fstIndex){
-                result[fstIndex + sndIndex] = snd[sndIndex++];
+        for (int i = 0; i < result.length; i++) {
+            if (fstIndex == fst.length) {
+                result[i] = snd[sndIndex++];
                 continue;
             }
-            if (snd.length == sndIndex){
-                result[fstIndex + sndIndex] = fst[fstIndex++];
+            if (sndIndex == snd.length) {
+                result[i] = fst[fstIndex++];
                 continue;
             }
-            if (snd[sndIndex] > fst[fstIndex]) {
-                result[fstIndex + sndIndex] = fst[fstIndex++];
+            if (fst[fstIndex] < snd[sndIndex]) {
+                result[i] = fst[fstIndex++];
             } else {
-                result[fstIndex + sndIndex] = snd[sndIndex++];
+                result[i] = snd[sndIndex++];
             }
         }
         return result;

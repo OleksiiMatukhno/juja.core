fd6fe28596b01022f67d14f31b221ede4b9032a8
OleksiiMatukhno <alexei.matukhno@gmail.com>
2018-10-17 16:05:33 +0300

    lab34(done)

--- src/ua/com/juja/core/lab34/BigIntegerUtils.java
+++ src/ua/com/juja/core/lab34/BigIntegerUtils.java
@@ -7,19 +7,25 @@ import java.util.Arrays;
 
 public class BigIntegerUtils {
     public static String toBitStr(BigInteger arg) {
-        String result = new String("");
         byte[] byteArray = arg.toByteArray();
-        for (int i = byteArray.length - 1; i >=0 ; i--) {
-            byte v = byteArray[i];
-            if (v < 0) {
-
+        if (byteArray.length == 1)
+            return Integer.toBinaryString(byteArray[0]);
+        String result = new String("");
+        for (int i = byteArray.length - 1; i >= 0; i--) {
+            String tmp = Integer.toBinaryString(byteArray[i]);
+            if (tmp.length() > 8) {
+                tmp = tmp.substring(tmp.length() - 8);
             }
-            String tmp = Integer.toBinaryString((byte)byteArray[i]);
             if (i > 0) {
                 while (tmp.length() < 8) {
                     tmp = '0' + tmp;
                 }
+            } else {
+                if (tmp.equals("0")) {
+                    return result;
+                }
             }
+
             result = tmp + result;
         }
         return result;
---
+++ src/ua/com/juja/core/lab34/TestZero.java
@@ -0,0 +1,17 @@
+package ua.com.juja.core.lab34;
+
+import java.math.BigInteger;
+
+public class TestZero {
+    public static void main(String[] args) {
+        BigInteger k = new BigInteger("0");
+        String expected = "0";
+        String actual = BigIntegerUtils.toBitStr(k);
+
+        if (!expected.equals(actual)) {
+            throw new AssertionError();
+        }
+
+        System.out.print("OK");
+    }
+}

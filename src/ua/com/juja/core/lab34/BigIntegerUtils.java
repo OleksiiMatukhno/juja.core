71b1e7adae2f1f62024eab33e1ed570d99d97410
OleksiiMatukhno <alexei.matukhno@gmail.com>
2018-10-22 17:17:53 +0300

    lab34(ref)

--- src/ua/com/juja/core/lab34/BigIntegerUtils.java
+++ src/ua/com/juja/core/lab34/BigIntegerUtils.java
@@ -1,20 +1,29 @@
 package ua.com.juja.core.lab34;
 
+import jdk.nashorn.internal.runtime.BitVector;
+import sun.security.util.BitArray;
+
 import java.math.BigInteger;
+import java.util.BitSet;
 
 public class BigIntegerUtils {
     public static String toBitStr(BigInteger arg) {
         byte[] byteArray = arg.toByteArray();
-        if (byteArray.length == 1)
-            return Integer.toBinaryString(byteArray[0]);
         int index = byteArray.length;
         String result = "";
-        while (--index > 0) {
+        while (--index >= 0) {
             String nextByte = Integer.toBinaryString(byteArray[index]);
-            result = ("00000000" + nextByte).substring(nextByte.length()) + result;
+            if (index != 0) {
+                result = ("00000000" + nextByte).substring(nextByte.length()) + result;
+                continue;
+            }
+            if (nextByte.length() > 8)
+                nextByte = nextByte.substring(nextByte.length() - 8);
+            if (byteArray[index] != 0)
+                return nextByte + result;
+            if (byteArray.length == 1)
+                return nextByte;
         }
-        if (byteArray[index] != 0)
-            return Integer.toBinaryString(byteArray[index]) + result;
         return result;
     }
 }

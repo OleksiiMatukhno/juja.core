0b187b26c967c41b61c35835780c1fe0a0a1d1a9
OleksiiMatukhno <alexei.matukhno@gmail.com>
2018-10-24 14:20:49 +0300

    lab36 ref

--- src/ua/com/juja/core/lab36/PolyUtils.java
+++ src/ua/com/juja/core/lab36/PolyUtils.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 public class PolyUtils {
     public static BigInteger[] mul(BigInteger[] x, BigInteger[] y) {
         BigInteger[] result = new BigInteger[x.length + y.length - 1];
-        for (int indexX = 0; indexX < x.length; indexX++) {
+        for (int indexX = 0; indexX < x.length; indexX++)
             for (int indexY = 0; indexY < y.length; indexY++) {
-                if (result[indexX + indexY] == null)
-                    result[indexX + indexY] = new BigInteger("0");
-                result[indexX + indexY] = result[indexX + indexY].add(x[indexX].multiply(y[indexY]));
+                int indexResult = indexX + indexY;
+                result[indexResult] = result[indexResult] == null
+                                    ? new BigInteger((x[indexX].multiply(y[indexY])).toString())
+                                    : result[indexResult].add(x[indexX].multiply(y[indexY]));
             }
-        }
         return result;
     }
 }
